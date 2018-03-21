d0ee87b086003b83c6e4f74535a02ba8a568beed
KaiyangZhou <k.zhou@qmul.ac.uk>
2018-03-21 17:14:46 +0000

    mkdir dirname to plot

--- main.py
+++ main.py
@@ -190,7 +190,10 @@ def plot_features(features, labels, num_classes, epoch, prefix):
             s=1,
         )
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
-    save_name = osp.join(args.save_dir, prefix, 'epoch_' + str(epoch+1) + '.png')
+    dirname = osp.join(args.save_dir, prefix)
+    if not osp.exists(dirname):
+        os.mkdir(dirname)
+    save_name = osp.join(dirname, 'epoch_' + str(epoch+1) + '.png')
     plt.savefig(save_name, bbox_inches='tight')
     plt.close()
 
